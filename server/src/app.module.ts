588fa0fdbd18451b638bd6334719b6b46b7f2c43
marcusaurelius80 <marcusaurelius80@gmail.com>
2023-09-12 23:01:32 +0900

    feat: TypeORM 세팅 for db연결

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from './config/orm.config';
 
 @Module({
-  imports: [OrdersModule],
+  imports: [
+    OrdersModule,
+    TypeOrmModule.forRootAsync({ useFactory: ormConfig }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
