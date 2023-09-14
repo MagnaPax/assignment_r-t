ffb0bd474dedc877f0056d09b23a7bdc2c913e62
marcusaurelius80 <marcusaurelius80@gmail.com>
2023-09-14 17:39:24 +0900

    feat: auth 컨트롤러에 핸들러 메서드 추가
    - 유저의 요청을 받아줄 핸들러 메서드

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,34 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request, Response } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async googleLogin(): Promise<void> {
+    // 이 부분은 브라우저에서 자동으로 Google 인증 페이지로 리디렉션
+    // 사용자가 인증 후 돌아오면 Passport에서 자동으로 콜백 메서드를 호출
   }
 
-  @Get()
-  findAll() {
-    return this.authService.findAll();
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  async googleLoginCallback(
+    @Request() req: any,
+    @Response() res: any,
+  ): Promise<void> {
+    const jwt: string = req.user.jwt;
+    if (jwt) res.redirect('http://localhost:5425/login/success/' + jwt);
+    else res.redirect('http://localhost:5425/login/failure');
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  @Get('protected')
+  @UseGuards(AuthGuard('jwt'))
+  protectedResource() {
+    return 'JWT is working!';
   }
 }
