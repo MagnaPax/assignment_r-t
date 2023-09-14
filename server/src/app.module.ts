e49dce475d5d2b6ea301d7f65f5821b2c68adf29
marcusaurelius80 <marcusaurelius80@gmail.com>
2023-09-14 16:18:36 +0900

    feat: Create resource of users

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,12 +7,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ormConfig } from './config/orm.config';
 import { DataSource } from 'typeorm';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     OrdersModule,
     TypeOrmModule.forRootAsync({ useFactory: ormConfig }),
     AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
